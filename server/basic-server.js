a11b162afd81bc0c7fca769b0c0b853725e53466
yntema <gerrityntema@gmail.com>
2016-02-02 11:37:31 -0800

    Refactor to Express.js

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,19 +2,22 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 var path = require("path");
+
+var express = require("express");
+
 var handler = require("./request-handler.js");
+
+var app = express();
+
+app.use(express.static(__dirname + "/../client/"));
+
+app.get('/classes/messages/', handler.requestHandler);
+app.get('/classes/room1/', handler.requestHandler);
+app.post('/classes/messages/', handler.requestHandler);
 
 var port = 3000;
 var ip = "127.0.0.1";
 
-fs.readFile('./client/index.html', function (error, html) {
-  if (error) { throw error; }
-  // var server = http.createServer(handler.requestHandler);
-  var server = http.createServer(function(request, response) {
-    response.writeHeader(200, {'Content-Type': 'text/html'});
-    response.write(html);
-    response.end();
-  });
-  console.log("Listening on http://" + ip + ":" + port);
-  server.listen(port, ip);
+app.listen(port, function() {
+  console.log("Listening on http://" + ip + ":" + port);  
 });
