09d83bb9685482c2f20e8b871a8b3d660b6562e1
yntema <gerrityntema@gmail.com>
2016-02-02 18:26:10 -0800

    Refactor code with functional abstractions and implement file write protocol

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,49 +1,56 @@
-  // http://nodejs.org/documentation/api/
-var requestHandler = function(request, response) {
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
-  var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = "application/json";
-
-  if (request.method === "OPTIONS") {
-    response.writeHead(200, 'OK', headers);
-    response.end();
-  } else if (request.method === "GET") {
-    if (request.url === "/classes/room1") {
-      response.writeHead(200, headers);
-      response.end(JSON.stringify({results:storage}));
-    } else if (request.url === "/classes/messages") {
-      response.writeHead(200, headers);
-      response.end(JSON.stringify({results:storage}));
-    } else {
-      response.writeHead(404, headers);
-      response.end();
-    }
-  } else if (request.method === "POST") {
-    var body = '';
-    request.on('data', function(chunk) {
-      body += chunk.toString();
-    });
-
-    request.on('end', function() {
-      response.writeHead(201, 'OK', headers);
-
-      var decodeBody = JSON.parse(body);
-      storage.push(decodeBody);
-
-      response.end(JSON.stringify({results:storage}));
-    });
-  }
-};
-
-var storage = [];
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var path = require("path");
   
-var defaultCorsHeaders = {
+var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "application/json"
 };
 
-exports.requestHandler = requestHandler;
+var objectID = 0;
+var storage = [];
+
+var sendResponse = function(response, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify({results:storage}));
+};
+
+var getHandler = function (request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  sendResponse(response, 200);
+};
+
+var postHandler = function (request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+
+  var buffer = '';
+  var file = './server/data.txt';
+  
+  request.on('data', function(chunk) {
+    buffer += chunk.toString();
+  });
+
+  request.on('end', function() {
+    var decodedBuffer = JSON.parse(buffer);
+    decodedBuffer.objectId = ++objectID;
+
+    fs.writeFile(file, buffer, function(error) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('File Write Successful');
+      }
+    });
+
+    storage.push(decodedBuffer);
+    sendResponse(response, 201);
+  });
+};
+
+exports.getHandler = getHandler;
+exports.postHandler = postHandler;
