1a0cd9012d190c9ab7f4578fff45b74b4dbfe2b4
yntema <gerrityntema@gmail.com>
2016-02-02 18:24:20 -0800

    Clean up server code using Express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,19 +1,18 @@
-var http = require("http");
-var fs = require("fs");
-var url = require("url");
-var path = require("path");
-
 var express = require("express");
-
 var handler = require("./request-handler.js");
 
 var app = express();
 
 app.use(express.static(__dirname + "/../client/"));
 
-app.get('/classes/messages/', handler.requestHandler);
-app.get('/classes/room1/', handler.requestHandler);
-app.post('/classes/messages/', handler.requestHandler);
+app.get('/classes/messages', function(request, response) {
+  handler.getHandler(request, response);
+});
+
+
+app.post('/classes/messages', function(request, response) {
+  handler.postHandler(request, response);
+});
 
 var port = 3000;
 var ip = "127.0.0.1";
